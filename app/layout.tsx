3e3dc96d537eb3c00267b361b73a156435d3e345
xf <xf@XdeMacBook-Air.local>
2025-06-16 20:45:44 +0800

    增加Google Analytics 功能

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,11 +17,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-                  <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8773372379395342"
-            crossOrigin="anonymous"
-          ></script>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WTG8PRNPZQ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WTG8PRNPZQ');
+            `,
+          }}
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8773372379395342"
+          crossOrigin="anonymous"
+        ></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
